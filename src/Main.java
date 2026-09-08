517490f370d418128ea9aef3a1005c17f8ed48cf
Kris Zebarth <kris.zebarth@ocsb.ca>
2026-09-07 20:00:19 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+/*******************************
+ * Name: Your name
+ * Class: ICS3U
+ * Date: When it's due
+ * Project Name:
+ * 
+ * Project Description
+ ********************************/
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		// YOUR CODE GOES HERE!!!!
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		in.close();//Don't forget to clean up after yourselves!
+	}
+
+}
